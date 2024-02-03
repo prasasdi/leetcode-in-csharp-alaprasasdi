fe7202892ad76a2b12411538d1e99abdea00d5d9
prasasdi <rizky.adie7@gmail.com>
2024-02-03 19:49:16 +0700

    doc: rewrite documentation in rotate method with better explanation, hope so i wish

--- CSLibraryExtension/CSLib.cs
+++ CSLibraryExtension/CSLib.cs
@@ -20,8 +20,18 @@ namespace CSLibraryExtension
         }
         public static void Rotate(int[] nums, int k)
         {
-            /**
-             * Buat pointer
+            /***
+             * Kuncinya adalah
+             * 
+             * Taruh nilai pada posisi setelah digeser k kali
+             * 
+             * Contoh dibawah pertama kali adalah 1 atau nums[0]
+             * Kita cari dimana nilai 1 atau nums[0] harusnya berada setelah dirotasi berapa k kali
+             * 
+             * Ternyata ada di index 3 nums[3], dapet dari (current + k) % length
+             * 
+             * Nah nilai sebelumnya dari index 3 nums[3] adalah 4. Dan dimana harusnya nilai 4 setelah dirotasi ke K kali?
+             * Nah itulah dilooping lagi
              */
             int moves = 0;
             int sweep = 0;
@@ -29,84 +39,48 @@ namespace CSLibraryExtension
             while (moves < nums.Length)
             {
                 /**
-                 * Ilustrasi
-                 * sweep = 0
-                 * current = sweep => current = 0
-                 * 
-                 * prev = temp = nums[sweep] => temp = nums[0]
+                 * curr sebagai pointer dalam loop do
+                 * dan prev adalah nilai yang mau kita geser
                  */
-                int curr = sweep; // kayak i dalam for
-                int prev = nums[sweep]; // karena var temp udah ada di dalam loop. jadi dibuat dua temp, dan ini salah satunya. Disebutlah prev
+                int curr = sweep; 
+                int prev = nums[sweep];
 
                 do
                 {
                     /**
-                     * nextIndex = (current + k) % nums.Length
-                     * sebutlah
-                     * 
-                     * nums = [1,2,3,4,5,6,7,8]
-                     * 
-                     * current = 0, k = 3, dan length = 8
-                     * ada modulo sebagai penyederhana kalau lebih dari Length
-                     * jadi hasilnya
-                     * nextIndex = 3 walaupun 3 mod 8 = 3
-                     * 
-                     * [1,2,3,4,5,6,7,8]
-                     *        ^nextIndex
-                     *        
-                     * [1,2,3,4,5,6,7,8]
-                     *  ^sweep = i
+                     * Cari nextIndex lalu taruh nums[nextIndex] ke variabel temp
                      */
                     int nextIndex = (curr + k) % nums.Length;
-
-                    /**
-                     * temp = nums[nextIndex] => temp = nums[3]
-                     * sementara temp ini sebagai temp dari kanan
-                     * 
-                     * temp = 4
-                     */
                     int temp = nums[nextIndex];
 
                     /**
-                     * masih tau kan nextIndex saat i = 0 berapa? nextIndex = 3
-                     * Nah disini ditiban sama nilai prev yang diatas tadi => prev = nums[0] => prev = 1
-                     * 
-                     * [1,2,3,1,5,6,7,8]
-                     *  ^prev ^nI
+                     * tempatkan prev di tempat setelah digeser k kali.
+                     * Contoh nilai pertama nums[0] setelah di geser k kali jadi nums[3]
                      */
                     nums[nextIndex] = prev;
 
                     /**
-                     * Inget ya, prev dan temp ini adalah "sama" dalam tanda kutip
-                     * yang asli beroperasi adalah prev sedangkan temp sebagai "pointer"ish
-                     * 
-                     * prev = 4 karena tadi temp = 4
+                     * kemana nums[3] sekarang? nanti dicari dilooping selanjutnya
+                     * sementara kita jadiin nilai nums[3] dalam temp ke variabel prev
                      */
                     prev = temp;
 
                     /**
-                     * pointer current = nextIndex
-                     * current = 3
-                     * nanti nextIndex dikasih nilai baru lagi diatas
+                     * jadiin nilai nextIndex sebagai posisi sekarang (current position)
                      */
                     curr = nextIndex;
 
+                    /**
+                     * inkremen pointer looping
+                     */
                     moves++;
-                    Console.WriteLine("do");
-                    PrintArray(nums);
-                } while (sweep != curr && moves < nums.Length); //sama kayak while (sweep != curr && moves < nums.Length)
+                } while (sweep != curr && moves < nums.Length);
+
                 /**
-                 * Diputaran pertama yuk liat dapet berapa
-                 * sweep = 0 nanti diinkremen kalau do while diatas selesai. but when?
-                 * curr = 3
-                 * moves = 1 karna udah diinkremen
-                 * nums.Length = 8 ya iyalah
+                 * Kalau dalam looping do-while diatas belum berhasil atau dalam satu momen kebetulan sweep == current
+                 * pointer sweep diinkremen. 
                  */
-
                 sweep++;
-                Console.WriteLine("out do");
-                PrintArray(nums);
-
             }
         }
     }
